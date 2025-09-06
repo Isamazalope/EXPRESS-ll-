bbb6ec0733b8ee799d07340ed78924c8ce276506
Isaias Mazariegos <isamazalope@gmail.com>
2025-09-06 08:16:11 -0600

    primer commit

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
 
-const path =require("path");
-app.use(express.static(path.join(__dirname,'public')));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine','ejs');
-app.set('views',path.join(__dirname,'scr/views'));
-const mainRouter=require("./scr/routes/main.routes");
-app.use(mainRouter)
-app.use('/productos',require('./scr/routes/productos.rauter'));
-const PORT = process.env.PORT ||3001;
-app.listen(PORT,() => console.log(`http://localhost:${PORT}`));
+app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'/src/views'));
+const mainRouter = require('./src/routes/main.routes');
+app.use(mainRouter);
+app.use('/productos',require('./src/routes/productos.router'));
+app.use('/usuarios',require('./src/routes/usuarios.router'));
+app.use('/carrito',require('./src/routes/carrito.router'));
+const port = process.env.PORT || 3001;
+app.listen(port, () =>console.log(`http://localhost:${port}`));
